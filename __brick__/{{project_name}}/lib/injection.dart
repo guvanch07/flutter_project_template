9edb2444d0511a2ebdbbb1538dd541ce6defb5e5
Guvanch Amanov <guvanch.amanov@skystart.team>
2025-12-05 18:44:47 +0300

    feat: update dependency injection with new repositories and cubits

--- __brick__/{{project_name}}/lib/injection.dart
+++ __brick__/{{project_name}}/lib/injection.dart
@@ -1,30 +1,78 @@
 import 'package:dio/dio.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:{{project_name}}/data/remote/api_client.dart';
 import 'package:{{project_name}}/data/repositories/auth_repository_impl_firebase.dart';
 import 'package:{{project_name}}/data/repositories/notification_repository_impl.dart';
-import 'package:{{project_name}}/data/repositories/user_repository_impl.dart';
+import 'package:{{project_name}}/data/repositories/preferences_repository_impl.dart';
+import 'package:{{project_name}}/data/repositories/secure_storage_repository_impl.dart';
+import 'package:{{project_name}}/data/repositories/storage_repository_impl.dart';
 import 'package:{{project_name}}/domain/repositories/auth_repository.dart';
 import 'package:{{project_name}}/domain/repositories/notification_repository.dart';
-import 'package:{{project_name}}/domain/repositories/user_repository.dart';
+import 'package:{{project_name}}/domain/repositories/preferences_repository.dart';
+import 'package:{{project_name}}/domain/repositories/secure_storage_repository.dart';
+import 'package:{{project_name}}/domain/repositories/storage_repository.dart';
 import 'package:{{project_name}}/internal/analytics/providers/firebase.dart';
 import 'package:{{project_name}}/internal/analytics/service.dart';
+import 'package:{{project_name}}/presentation/cubits/connectivity/connectivity_cubit.dart';
+import 'package:{{project_name}}/presentation/cubits/log/log_cubit.dart';
+import 'package:{{project_name}}/presentation/cubits/theme/theme_cubit.dart';
+import 'package:{{project_name}}/presentation/utils/connectivity_service.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> configureDependencies() async {
-  // External
+  // External dependencies
+  final sharedPreferences = await SharedPreferences.getInstance();
+  getIt.registerSingleton<SharedPreferences>(sharedPreferences);
+
   getIt.registerLazySingleton<Dio>(() => Dio());
+  getIt.registerLazySingleton<FirebaseStorage>(() => FirebaseStorage.instance);
+  getIt.registerLazySingleton<FlutterSecureStorage>(
+    () => const FlutterSecureStorage(),
+  );
 
   // Services
   getIt.registerLazySingleton<ApiClient>(() => ApiClient(getIt<Dio>()));
-  getIt.registerLazySingleton<AnalyticsService>(() => FirebaseAnalyticsProvider());
+  getIt.registerLazySingleton<AnalyticsService>(
+    () => FirebaseAnalyticsProvider(),
+  );
+  getIt.registerLazySingleton<ConnectivityService>(
+    () => ConnectivityService.instance,
+  );
 
   // Repositories
-  getIt.registerLazySingleton<UserRepository>(() => UserRepositoryImpl(getIt<ApiClient>()));
-  getIt.registerLazySingleton<AuthRepository>(() => AuthRepositoryImplFirebase());
-  getIt.registerLazySingleton<NotificationRepository>(() => NotificationRepositoryImpl());
-  
-  // Blocs/Cubits - Registered as factories usually, or created in UI
+  getIt.registerLazySingleton<PreferencesRepository>(
+    () => PreferencesRepositoryImpl(
+      sharedPreferences: getIt<SharedPreferences>(),
+    ),
+  );
+  getIt.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImplFirebase(),
+  );
+  getIt.registerLazySingleton<NotificationRepository>(
+    () => NotificationRepositoryImpl(),
+  );
+  getIt.registerLazySingleton<StorageRepository>(
+    () => StorageRepositoryImpl(storage: getIt<FirebaseStorage>()),
+  );
+  getIt.registerLazySingleton<SecureStorageRepository>(
+    () => SecureStorageRepositoryImpl(
+      secureStorage: getIt<FlutterSecureStorage>(),
+    ),
+  );
+
+  // Cubits - Registered as singletons for app-wide state
+  getIt.registerLazySingleton<ThemeCubit>(
+    () => ThemeCubit(preferencesRepository: getIt<PreferencesRepository>()),
+  );
+  getIt.registerLazySingleton<LogCubit>(() => LogCubit());
+  getIt.registerLazySingleton<ConnectivityCubit>(
+    () => ConnectivityCubit(connectivityService: getIt<ConnectivityService>()),
+  );
+
+  // Feature-specific cubits can be registered as factories
   // getIt.registerFactory(() => HomeCubit());
 }
